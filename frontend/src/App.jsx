bff28441ab788f855e2875552805a139d6999c42
Dasun Sandepa <dsk.sandeep.987@gmail.com>
2025-09-03 10:36:12 +0530

    Updated work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,23 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Movie Application</h1>
+      {/* <h1>Movie Application</h1>
       <Test props="hello" />
-      <Test props="hello world" />
+      <Test props="hello world" /> */}
+
+
     </>
   );
 }
 
-function Test({ props }) {
-  return (
-    <>
-      <h4>{props}</h4>
-    </>
-  );
-}
+
+
+// function Test({ props }) {
+//   return (
+//     <>
+//       <h4>{props}</h4>
+//     </>
+//   );
+// }
 
 export default App;
